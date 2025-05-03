47736e43920518114b7641c53ff09b6ad54098fc
duc-anh-le <ducanhle.iter@gmail.com>
2025-05-03 16:30:56 +0700

    refactor: update system architecture

--- MamaFit/MamaFit.API/DependencyInjection/ServiceRegistration.cs
+++ MamaFit/MamaFit.API/DependencyInjection/ServiceRegistration.cs
@@ -1,22 +1,77 @@
 ﻿using MamaFit.Services.Mapper;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MamaFit.Configuration
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection Register(this IServiceCollection services)
+        public static IServiceCollection AddHttpClientServices(this IServiceCollection services)
         {
-            //Configure AutoMapper
+            services.AddHttpClient();
+            return services;
+        }
+
+        public static IServiceCollection AddConfigSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo() { Title = "MamaFit System", Version = "v1" });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+                });
+                options.MapType<TimeOnly>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Format = "time",
+                    Example = OpenApiAnyFactory.CreateFromJson("\"13:45:42\"")
+                });
+            });
+            return services;
+        }
+
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper();
+            services.AddRepositories();
+            services.AddServices();
+            services.AddAutoMapper();
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            // Add your repository registrations here
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+            // Add your service registrations here
+        }
+
+        private static void AddAutoMapper(this IServiceCollection services)
+        {
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddAutoMapper(typeof(MapperEntities).Assembly);
-
-            #region DependencyInjection
-
-            //External services
-            #endregion
-
-            return services;
-
         }
     }
 }

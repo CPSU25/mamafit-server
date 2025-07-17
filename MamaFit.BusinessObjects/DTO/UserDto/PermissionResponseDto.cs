776fd99e2e7168ae931ec57bbbbde3fa44061300
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-07-17 15:00:14 +0700

    Update PermissionResponseDto.cs

--- MamaFit.BusinessObjects/DTO/UserDto/PermissionResponseDto.cs
+++ MamaFit.BusinessObjects/DTO/UserDto/PermissionResponseDto.cs
@@ -2,7 +2,7 @@ namespace MamaFit.BusinessObjects.DTO.UserDto;
 
 public class PermissionResponseDto
 {
-    public string UserName { get; set; 
+    public string UserName { get; set; }
     public string UserEmail { get; set; }
     public string RoleName { get; set; }
     public string ProfilePicture { get; set; }

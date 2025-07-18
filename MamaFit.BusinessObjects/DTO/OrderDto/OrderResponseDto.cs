033ac0a3e8054e68db09d15d40f7d383bda80fe0
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-07-19 01:52:41 +0700

    fix: order response

--- MamaFit.BusinessObjects/DTO/OrderDto/OrderResponseDto.cs
+++ MamaFit.BusinessObjects/DTO/OrderDto/OrderResponseDto.cs
@@ -7,6 +7,9 @@ public class OrderResponseDto : OrderBaseDto
     public string Id { get; set; }
     public Address? Address { get; set; } = new Address();
     public string? Code { get; set; }
+    public decimal? DiscountSubtotal { get; set; }
+    public decimal? DepositSubtotal { get; set; }
+    public decimal? RemainingBalance { get; set; }
     public string? CreatedBy { get; set; }
     public string? UpdatedBy { get; set; }
     public DateTime CreatedAt { get; set; }

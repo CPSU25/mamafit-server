c3b6cb076f9196dfba5e2026082e02d9bc56ff5a
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-08-05 15:40:59 +0700

    feat: integration cms and notify in create warranty

--- MamaFit.BusinessObjects/DTO/WarrantyRequestDto/WarrantyRequestGetAllDto.cs
+++ MamaFit.BusinessObjects/DTO/WarrantyRequestDto/WarrantyRequestGetAllDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MamaFit.BusinessObjects.DTO.WarrantyRequestDto
+﻿namespace MamaFit.BusinessObjects.DTO.WarrantyRequestDto
 {
     public class WarrantyRequestGetAllDto : WarrantyBaseDto
     {
--- MamaFit.Services/Service/WarrantyRequestService.cs
+++ MamaFit.Services/Service/WarrantyRequestService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MamaFit.BusinessObjects.DTO.NotificationDto;
 using MamaFit.BusinessObjects.DTO.WarrantyRequestDto;
 using MamaFit.BusinessObjects.Entity;
 using MamaFit.BusinessObjects.Enum;
@@ -14,15 +15,21 @@ namespace MamaFit.Services.Service
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IValidationService _validationService;
+        private readonly INotificationService _notificationService;
+        private readonly IConfigService _configService;
 
         public WarrantyRequestService(
             IUnitOfWork unitOfWork,
             IMapper mapper,
-            IValidationService validationService)
+            IValidationService validationService,
+            INotificationService notificationService,
+            IConfigService configService)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _validationService = validationService;
+            _notificationService = notificationService;
+            _configService = configService;
         }
 
         public async Task CreateAsync(WarrantyRequestCreateDto warrantyRequestCreateDto)
@@ -30,17 +37,30 @@ namespace MamaFit.Services.Service
             var rootOrderItem = await GetRootOrderItemAsync(warrantyRequestCreateDto.WarrantyOrderItemId!);
             _validationService.CheckNotFound(rootOrderItem, "Root order item don't exist!");
 
-            var rootOrder = await _unitOfWork.OrderRepository.GetByIdNotDeletedAsync(rootOrderItem.OrderId);
+            var rootOrder = await _unitOfWork.OrderRepository.GetByIdNotDeletedAsync(rootOrderItem.OrderId!);
             _validationService.CheckNotFound(rootOrder, "Order don't exist!");
 
-            if (rootOrder.PaymentStatus != PaymentStatus.PAID_FULL &&
+            if (rootOrder!.PaymentStatus != PaymentStatus.PAID_FULL &&
                 rootOrder.PaymentStatus != PaymentStatus.PAID_DEPOSIT_COMPLETED)
                 throw new ErrorException(StatusCodes.Status400BadRequest, ApiCodes.BAD_REQUEST,
                     "Order must be paid before requesting warranty!");
 
+            var config = await _configService.GetConfig();
+
             var warrantyRequestCount =
                 await _unitOfWork.WarrantyRequestRepository.CountWarrantyForOrderItemAsync(rootOrderItem.Id);
 
+            if (warrantyRequestCount >= config.Fields?.WarrantyTime)
+                throw new ErrorException(StatusCodes.Status400BadRequest, ApiCodes.BAD_REQUEST,
+                    $"You have reached the maximum warranty requests ({config.Fields?.WarrantyTime}) for this item!");
+
+            if (rootOrder.ReceivedAt == null)
+                throw new ErrorException(StatusCodes.Status400BadRequest, ApiCodes.BAD_REQUEST,
+                    "Order must be received before requesting warranty!");
+            var daysSinceReceived = (DateTime.UtcNow - rootOrder.ReceivedAt.Value).TotalDays;
+            if (daysSinceReceived > config.Fields?.WarrantyPeriod)
+                throw new ErrorException(StatusCodes.Status400BadRequest, ApiCodes.BAD_REQUEST,
+                    $"The product is out of warranty (more than {config.Fields.WarrantyPeriod} days since received)!");
             var newOrder = new Order
             {
                 UserId = rootOrder.UserId,
@@ -58,7 +78,7 @@ namespace MamaFit.Services.Service
             var newOrderItem = new OrderItem
             {
                 OrderId = newOrder.Id,
-                ParentOrderItemId = rootOrderItem.Id, 
+                ParentOrderItemId = rootOrderItem.Id,
                 MaternityDressDetailId = rootOrderItem.MaternityDressDetailId,
                 PresetId = rootOrderItem.PresetId,
                 ItemType = ItemType.WARRANTY,
@@ -77,9 +97,22 @@ namespace MamaFit.Services.Service
             };
             await _unitOfWork.WarrantyRequestRepository.InsertAsync(warrantyRequest);
             await _unitOfWork.SaveChangesAsync();
+            await _notificationService.SendAndSaveNotificationAsync(new NotificationRequestDto
+            {
+                ReceiverId = rootOrder.UserId,
+                NotificationTitle = "New Warranty Request",
+                NotificationContent =
+                    $"Your warranty request for order item {rootOrderItem.Preset?.Name ?? rootOrderItem.MaternityDressDetail?.Name} has been created successfully.",
+                ActionUrl = $"/warranty-requests/{warrantyRequest.Id}",
+                Metadata = new Dictionary<string, string>
+                {
+                    { "warrantyRequestId", warrantyRequest.Id },
+                    { "orderItemId", rootOrderItem.Id },
+                    { "orderId", newOrder.Id }
+                }
+            });
         }
-
-
+        
         public async Task DeleteAsync(string id)
         {
             var warrantyRequest = await _unitOfWork.WarrantyRequestRepository.GetByIdAsync(id);
@@ -131,7 +164,7 @@ namespace MamaFit.Services.Service
         private async Task<OrderItem> GetRootOrderItemAsync(string itemId)
         {
             var item = await _unitOfWork.OrderItemRepository.GetByIdNotDeletedAsync(itemId);
-            while (!string.IsNullOrEmpty(item.ParentOrderItemId))
+            while (!string.IsNullOrEmpty(item!.ParentOrderItemId))
             {
                 item = await _unitOfWork.OrderItemRepository.GetByIdNotDeletedAsync(item.ParentOrderItemId);
             }

94c7fb539ae7415ebbe4f8a9eb8623ff3bbddbc9
thiennlse <thiennlse172858@gmail.com>
2025-08-06 14:07:48 +0700

    update: orderService to order many preset at same time

--- MamaFit.BusinessObjects/DTO/OrderDto/OrderPresetCreateRequestDto.cs
+++ MamaFit.BusinessObjects/DTO/OrderDto/OrderPresetCreateRequestDto.cs
@@ -1,17 +1,16 @@
-﻿using MamaFit.BusinessObjects.DTO.AddOnDto;
+﻿using MamaFit.BusinessObjects.DTO.PresetDto;
 using MamaFit.BusinessObjects.Enum;
 
 namespace MamaFit.BusinessObjects.DTO.OrderDto
 {
     public class OrderPresetCreateRequestDto
     {
-        public string? PresetId { get; set; }
         public string? AddressId { get; set; }
-        public string? BranchId { get; set; }
-        public List<AddOnOrderItemRequestDto>? Options { get; set; }
-        public decimal ShippingFee { get; set; } = 0;
+        public string? BranchId { get; set; }   
         public string? VoucherDiscountId { get; set; }
         public string? MeasurementId { get; set; }
+        public List<PresetListIdRequestDto> Presets { get; set; } = new List<PresetListIdRequestDto>();
+        public decimal ShippingFee { get; set; } = 0;
         public bool IsOnline { get; set; } = true;
         public PaymentMethod PaymentMethod { get; set; }
         public PaymentType PaymentType { get; set; }
---
+++ MamaFit.BusinessObjects/DTO/PresetDto/PresetListIdRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using MamaFit.BusinessObjects.DTO.AddOnDto;
+namespace MamaFit.BusinessObjects.DTO.PresetDto
+{
+    public class PresetListIdRequestDto
+    {
+        public string? Id { get; set; }
+        public List<AddOnOrderItemRequestDto>? Options { get; set; }
+    }
+}

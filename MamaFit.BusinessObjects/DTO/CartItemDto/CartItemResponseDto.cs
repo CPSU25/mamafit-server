673ca1e2456b8fdd6ab4bec719aa3677e6a319ed
thiennlse <thiennlse172858@gmail.com>
2025-08-07 13:34:48 +0700

    modify response include item

--- MamaFit.BusinessObjects/DTO/CartItemDto/CartItemResponseDto.cs
+++ MamaFit.BusinessObjects/DTO/CartItemDto/CartItemResponseDto.cs
@@ -1,6 +1,10 @@
+using MamaFit.BusinessObjects.DTO.MaternityDressDetailDto;
+using MamaFit.BusinessObjects.DTO.PresetDto;
+
 namespace MamaFit.BusinessObjects.DTO.CartItemDto;
 
 public class CartItemResponseDto : CartItemRequestDto
 {
-
+    public PresetGetAllResponseDto? Preset { get; set; }
+    public MaternityDressDetailResponseDto? MaternityDressDetail { get; set; }
 }

24139733ab0f9ea3b566b4469bfb955fb49f986c
thiennlse <thiennlse172858@gmail.com>
2025-08-08 10:47:57 +0700

    modify dto for sku for preset and maternity dress detail

--- MamaFit.BusinessObjects/DTO/MaternityDressDetailDto/MaternityDressDetailRequestDto.cs
+++ MamaFit.BusinessObjects/DTO/MaternityDressDetailDto/MaternityDressDetailRequestDto.cs
@@ -7,6 +7,7 @@
         public string? Description { get; set; }
         public string? Color { get; set; }
         public string? Image { get; set; }
+        public string? SKU { get; set; }
         public string? Size { get; set; }
         public float Price { get; set; }
         public int Quantity { get; set; }
--- MamaFit.BusinessObjects/DTO/PresetDto/PresetBaseRequestDto.cs
+++ MamaFit.BusinessObjects/DTO/PresetDto/PresetBaseRequestDto.cs
@@ -6,6 +6,7 @@ namespace MamaFit.BusinessObjects.DTO.PresetDto
     public class PresetBaseRequestDto
     {
         public List<string>? Images { get; set; }
+        public string? SKU { get; set; }
         public PresetType Type { get; set; }
         public bool IsDefault { get; set; } = false;
         public float Price { get; set; } 

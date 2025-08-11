e8fc6bb30983c978e66b5fe49744643a8afaf957
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-08-11 15:44:20 +0700

    feat: dashboard transaction

--- MamaFit.BusinessObjects/DTO/TransactionDto/TransactionResponseDto.cs
+++ MamaFit.BusinessObjects/DTO/TransactionDto/TransactionResponseDto.cs
@@ -1,8 +1,21 @@
 namespace MamaFit.BusinessObjects.DTO.TransactionDto;
 
-public class TransactionResponseDto : TransactionBaseDto
+public class TransactionResponseDto
 {
     public string Id { get; set; }
+    public string? OrderId { get; set; }
+    public string? SepayId { get; set; }
+    public string? Gateway { get; set; }
+    public DateTime? TransactionDate { get; set; }
+    public string? AccountNumber { get; set; }
+    public string? Code { get; set; }
+    public string? Content { get; set; }
+    public string? TransferType { get; set; }
+    public float? TransferAmount { get; set; }
+    public string? Accumulated { get; set; }
+    public string? SubAccount { get; set; }
+    public string? ReferenceCode { get; set; }
+    public string? Description { get; set; }
     public string? CreatedBy { get; set; }
     public string? UpdatedBy { get; set; }
     public DateTime CreatedAt { get; set; }

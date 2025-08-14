2918a14bdfbaafdc774cfe3108fa5edb8aa4eb8b
thiennlse <thiennlse172858@gmail.com>
2025-08-14 11:33:40 +0700

    update: response dto for warranty item

--- MamaFit.BusinessObjects/DTO/WarrantyRequestItemDto/WarrantyRequestItemGetAllDto.cs
+++ MamaFit.BusinessObjects/DTO/WarrantyRequestItemDto/WarrantyRequestItemGetAllDto.cs
@@ -14,6 +14,7 @@ public class WarrantyRequestItemGetAllDto
     public string? RejectedReason { get; set; }
     public string? Description { get; set; }
     public List<string>? Images { get; set; } = [];
+    public List<string>? Videos { get; set; } = [];
     public string? Status { get; set; }
     public DateTime? EstimateTime { get; set; }
     public DestinationType DestinationType { get; set; }

19621040f68105045fe39806a4ad5ade9574b650
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-08-16 13:23:56 +0700

    feat: add job title for staff

--- MamaFit.BusinessObjects/DTO/AuthDto/SystemAccountRequestDto.cs
+++ MamaFit.BusinessObjects/DTO/AuthDto/SystemAccountRequestDto.cs
@@ -4,6 +4,7 @@ public class SystemAccountRequestDto
 {
     public string UserName { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
+    public string JobTitle { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string UserEmail { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
